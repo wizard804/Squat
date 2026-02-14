c31b5ab886486875c9f4297d92d9b34b7d1cd3c9
wizard804 <lennydelrue86@gmail.com>
2026-02-14 18:36:31 +0100

    Update Firebase configuration details

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 const firebaseConfig = {
-    apiKey: "TON_API_KEY",
-    authDomain: "ton-projet.firebaseapp.com",
-    projectId: "ton-projet",
-    storageBucket: "ton-projet.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyDxTEoyx7uwDy1Hwhloa1wCVpYa5lJ4_9I",
+  authDomain: "squat-277ee.firebaseapp.com",
+  databaseURL: "https://squat-277ee-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "squat-277ee",
+  storageBucket: "squat-277ee.firebasestorage.app",
+  messagingSenderId: "646360838023",
+  appId: "1:646360838023:web:4fc76054717fed4b93ed22",
+  measurementId: "G-RM8N0YQKBS"
 };
 
 firebase.initializeApp(firebaseConfig);
